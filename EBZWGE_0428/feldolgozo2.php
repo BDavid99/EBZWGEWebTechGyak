4ff911c3d36d75874bf18efd68880cf7b65ac24e
BDavid99 <bodnardavid28@gmail.com>
2021-04-28 11:56:48 +0200

    EBZ

---
+++ EBZWGE_0428/feldolgozo2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Űrlap vezérlése új elemekkel</title>
+ </head>
+ <body>
+  
+<?php
+
+
+
+print ("Név: " . $_GET['nev'] . "<br>");
+
+
+
+print ("PIN kód: " . $_GET['kod'] . "<br>");
+
+
+
+
+
+?>
+
+
+
+ </body>
+</html>
